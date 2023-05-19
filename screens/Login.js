b8fd2edd93fd18b93d79cfa16a4c4c674eaf6e15
Berkay Aslan <73855743+KahlerYasla@users.noreply.github.com>
2023-05-19 12:52:00 +0300

    Login screen done with APIs

--- screens/Login.js
+++ screens/Login.js
@@ -1,21 +1,69 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Modal } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 function Login() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [fullName, setFullName] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [modalVisible, setModalVisible] = useState(false);
   const navigation = useNavigation();
 
-  const handleLogin = () => {
-    if (username === 'admin' && password === 'admin') {
-      navigation.navigate('HomeStackNavigator');
-    }
+  const handleLogin = async () => {
+    // Perform the login logic here, e.g., call an API endpoint
+    // Pass the username and password to the login endpoint
+    try {
+      const response = await fetch('http://localhost:5060/api/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      });
 
-    // Add your authentication logic here
-    // You can validate the username and password and navigate to the home screen if successful
-    if (username && password) {
-      navigation.navigate('HomeStackNavigator');
+      if (response.ok) {
+        // Handle successful login
+        navigation.navigate('Home');
+      } else {
+        // Handle login error, e.g., display an error message
+        console.log('Login failed');
+      }
+    } catch (error) {
+      console.log('Error:', error);
+    }
+  };
+
+  const handleRegister = () => {
+    setModalVisible(true);
+  };
+
+  const handleModalClose = () => {
+    setModalVisible(false);
+  };
+
+  const handleSave = async () => {
+    // Perform the user registration logic here, e.g., call an API endpoint
+    // Pass the registration data to the registration endpoint
+    try {
+      const response = await fetch('http://localhost:5060/api/user', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password, fullName, phoneNumber }),
+      });
+
+      if (response.ok) {
+        // Handle successful registration
+        setModalVisible(false);
+        // Optionally, you can clear the input fields here
+      } else {
+        // Handle registration error, e.g., display an error message
+        console.log('Registration failed');
+      }
+    } catch (error) {
+      console.log('Error:', error);
     }
   };
 
@@ -35,9 +83,53 @@ function Login() {
         value={password}
         onChangeText={(text) => setPassword(text)}
       />
-      <TouchableOpacity style={styles.button} onPress={handleLogin}>
-        <Text style={styles.buttonText}>Login</Text>
-      </TouchableOpacity>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button} onPress={handleLogin}>
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={handleRegister}>
+          <Text style={styles.buttonText}>Register</Text>
+        </TouchableOpacity>
+      </View>
+
+      <Modal visible={modalVisible} animationType="fade">
+        <View style={styles.modalContainer}>
+          <Text style={styles.modalTitle}>Registration</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Full Name"
+            value={fullName}
+            onChangeText={(text) => setFullName(text)}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Username"
+            value={username}
+            onChangeText={(text) => setUsername(text)}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Phone Number"
+            value={phoneNumber}
+            onChangeText={(text) => setPhoneNumber(text)}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Password"
+            secureTextEntry
+            value={password}
+            onChangeText={(text) => setPassword(text)}
+          />
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity style={styles.button} onPress={handleSave}>
+              <Text style={styles.buttonText}>Save</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={handleModalClose}>
+              <Text style={styles.buttonText}>Close</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
@@ -63,16 +155,32 @@ const styles = StyleSheet.create({
     marginBottom: 16,
     paddingLeft: 8,
   },
+  buttonContainer: {
+    flexDirection: 'row',
+    gap: 8,
+  },
   button: {
     backgroundColor: '#e91e63',
     paddingVertical: 12,
     paddingHorizontal: 24,
     borderRadius: 8,
+    marginBottom: 8,
   },
   buttonText: {
     color: '#fff',
     fontSize: 16,
     fontWeight: 'bold',
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+  },
+  modalTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 16,
   },
 });
 
