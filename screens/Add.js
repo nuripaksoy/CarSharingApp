85f56888526b75f2345797d6a21cbeb7466e9694
Berkay Aslan <73855743+KahlerYasla@users.noreply.github.com>
2023-05-20 15:31:37 +0300

    home and add only remained api connections;

--- screens/Add.js
+++ screens/Add.js
@@ -2,15 +2,25 @@ import React, { useState } from "react";
 import { Text, View, TouchableOpacity, StyleSheet } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import DateTimePicker from "@react-native-community/datetimepicker";
-import DropDownPicker from 'react-native-dropdown-picker';
+import DropDownPicker from "react-native-dropdown-picker";
+import Cities from "../Constants/cities";
+import { Slider } from "react-native-elements";
 
 function Add() {
     const [showDatePicker, setShowDatePicker] = useState(false);
-    const [dateRange, setDateRange] = useState("");
-    const [destination, setDestination] = useState("");
-    const [startingPoint, setStartingPoint] = useState("");
+    const [selectedDate, setSelectedDate] = useState(null);
+    const [price, setPrice] = useState(0);
 
-    const cities = ['New York', 'London', 'Paris', 'Tokyo'];
+    // dropdown items
+    const [startingPointOpen, setStartingPointOpen] = useState(false);
+    const [startingPointValue, setStartingPointValue] = useState(null);
+    const [startingPointItems, setStartingPointItems] = useState(Cities);
+
+    const [destinationOpen, setDestinationOpen] = useState(false);
+    const [destinationValue, setDestinationValue] = useState(null);
+    const [destinationItems, setDestinationItems] = useState(Cities);
+
+    const cities = ["New York", "London", "Paris", "Tokyo"];
 
     const handleSaveRoute = () => {
         // Perform save route logic
@@ -19,10 +29,9 @@ function Add() {
 
     const handleDateChange = (event, selectedDate) => {
         setShowDatePicker(false);
-        // Format the selected date and set it as the date range value
         if (selectedDate) {
-            const formattedDate = selectedDate.toLocaleDateString("en-US");
-            setDateRange(formattedDate);
+            setSelectedDate(selectedDate);
+            console.log(selectedDate);
         }
     };
 
@@ -30,103 +39,116 @@ function Add() {
         setShowDatePicker(true);
     };
 
+    const handlePriceChange = (value) => {
+        setPrice(value);
+    };
+
     return (
         <View style={styles.container}>
             <View style={{ flexDirection: "row" }}>
                 <MaterialCommunityIcons name="car" size={24} color="#e91e63" />
                 <View style={{ width: 8 }} />
-                <Text style={styles.title}>
-                    Add a Route
-                </Text>
+                <Text style={styles.title}>Add a Route</Text>
             </View>
             <View style={styles.form}>
-
                 <TouchableOpacity style={styles.input} onPress={showDatePickerModal}>
                     <MaterialCommunityIcons name="calendar" size={24} color="#e91e63" />
                     <Text style={styles.inputText}>
                         <View style={{ width: 5 }} />
-                        {dateRange ? dateRange : "Select Date"}
+                        {selectedDate ? selectedDate.toLocaleDateString("en-US") : "Select Date"}
                     </Text>
                 </TouchableOpacity>
+
 
                 {showDatePicker && (
                     <DateTimePicker
                         value={new Date()}
                         mode="date"
-                        display="spinner"
+                        display="default"
                         onChange={handleDateChange}
+
                     />
                 )}
 
                 <DropDownPicker
+                    open={startingPointOpen}
+                    value={startingPointValue}
+                    items={startingPointItems}
+                    setOpen={setStartingPointOpen}
+                    setValue={setStartingPointValue}
+                    setItems={setStartingPointItems}
+                    zIndex={1200}
+                    style={{
+                        borderColor: "black",
+                        borderWidth: 2,
+                        borderRadius: 8,
+                        marginBottom: 8,
+                    }}
                     placeholder={
-                        <View
-                            style={{
-                                flexDirection: "row",
-                                alignItems: "center",
-                            }}
-                        >
-                            <MaterialCommunityIcons name="map-marker" size={24} color="#e91e63" />
-                            <Text style={{ color: '#000', fontWeight: 'bold' }}> Destination</Text>
+                        <View style={{ flexDirection: "row", alignItems: "center" }}>
+                            <MaterialCommunityIcons
+                                name="map-marker"
+                                size={24}
+                                color="#e91e63"
+                            />
+                            <Text style={{ fontWeight: "bold" }}> Starting Point</Text>
                         </View>
                     }
-                    ArrowDownIconComponent={({ style }) => (
-                        <MaterialCommunityIcons name="arrow-down-drop-circle-outline" size={24} color="#e91e63" />
-                    )}
-                    ArrowUpIconComponent={({ style }) => (
-                        <MaterialCommunityIcons name="arrow-up-drop-circle-outline" size={24} color="#e91e63" />
-                    )}
-                    textStyle={{ color: '#000' }}
-                    items={cities.map((city) => ({ label: city, value: city }))}
-                    defaultValue={destination}
-                    containerStyle={{ height: "auto", marginBottom: 8, borderColor: "black", borderWidth: 2, borderRadius: 8 }}
-                    style={{ borderWidth: 0 }}
-                    placeholderStyle={{ color: '#000', fontWeight: 'bold' }}
-                    dropDownStyle={{ backgroundColor: "#fafafa" }}
-                    onChangeItem={(item) => setDestination(item.value)}
                 />
-
                 <DropDownPicker
+                    open={destinationOpen}
+                    value={destinationValue}
+                    items={destinationItems}
+                    setOpen={setDestinationOpen}
+                    setValue={setDestinationValue}
+                    setItems={setDestinationItems}
+                    zIndex={1100}
+                    style={{
+                        borderColor: "black",
+                        borderWidth: 2,
+                        borderRadius: 8,
+                        marginBottom: 8,
+                    }}
                     placeholder={
-                        <View
-
-                            style={{
-                                flexDirection: "row",
-                                alignItems: "center",
-                            }}
-                        >
-                            <MaterialCommunityIcons name="map-marker-path" size={24} color="#e91e63" />
-                            <Text style={{ color: '#000', fontWeight: 'bold' }}> Starting Point</Text>
+                        <View style={{ flexDirection: "row", alignItems: "center" }}>
+                            <MaterialCommunityIcons
+                                name="map-marker-path"
+                                size={24}
+                                color="#e91e63"
+                            />
+                            <Text style={{ fontWeight: "bold" }}> Destination</Text>
                         </View>
                     }
-                    ArrowDownIconComponent={({ style }) => (
-                        <MaterialCommunityIcons name="arrow-down-drop-circle-outline" size={24} color="#e91e63" />
-                    )}
-                    ArrowUpIconComponent={({ style }) => (
-                        <MaterialCommunityIcons name="arrow-up-drop-circle-outline" size={24} color="#e91e63" />
-                    )}
-                    textStyle={{ color: '#000' }}
-                    placeholderStyle={{ color: '#000', fontWeight: 'bold' }}
-                    items={cities.map((city) => ({ label: city, value: city }))}
-                    defaultValue={startingPoint}
-                    containerStyle={{ height: "auto", borderColor: "black", borderWidth: 2, borderRadius: 8 }}
-                    style={{ borderWidth: 0 }}
-                    dropDownStyle={{ backgroundColor: "#fafafa" }}
-                    onChangeItem={(item) => setStartingPoint(item.value)}
                 />
 
                 <View style={{ height: 8 }} />
 
                 {/* price */}
                 <View style={styles.input}>
-                    <MaterialCommunityIcons name="currency-usd" size={24} color="#e91e63" />
+                    <MaterialCommunityIcons
+                        name="currency-usd"
+                        size={24}
+                        color="#e91e63"
+                    />
                     <Text style={styles.inputText}>
                         <View style={{ width: 5 }} />
-                        Price
+                        Price: ${price.toFixed(2)}
                     </Text>
                 </View>
 
-                <TouchableOpacity style={styles.saveButton} onPress={handleSaveRoute}>
+                <Slider
+                    value={price}
+                    onValueChange={handlePriceChange}
+                    minimumValue={0}
+                    maximumValue={1000}
+                    step={1}
+                    thumbStyle={{ height: 20, width: 20, backgroundColor: "#e91e63" }}
+                />
+
+                <TouchableOpacity
+                    style={styles.saveButton}
+                    onPress={handleSaveRoute}
+                >
                     <Text style={styles.saveButtonText}>Save </Text>
                     <MaterialCommunityIcons name="map-plus" size={24} color="#fff" />
                 </TouchableOpacity>
